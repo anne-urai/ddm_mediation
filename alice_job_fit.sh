ba92bbc879b2c5d440dc92d4448a3728abfdc0bc
Anne Urai <a.e.urai@fsw.leidenuniv.nl>
2022-03-14 15:11:11 +0100

    trying to get GPU jobs to work

--- alice_job_fit.sh
+++ alice_job_fit.sh
@@ -1,20 +1,53 @@
 #!/bin/bash
 
-# https://wiki.alice.universiteitleiden.nl/index.php?title=Running_jobs_on_ALICE
+# https://wiki.alice.universiteitleiden.nl/index.php?title=Your_first_GPU_job
 #SBATCH --output /home/uraiae/jobs/job_fit-%A_%a.out
 #SBATCH --mail-user=a.e.urai@fsw.leidenuniv.nl # mail when done
 #SBATCH --mail-type=END,FAIL # mail when done
-#SBATCH -p gpu-short
-#SBATCH -t 1:00:00 # maximum 5 days on cartesius
-
-$SLURM_ARRAY_TASK_ID
+#SBATCH -p testing # gpu-short
+#SBATCH --ntasks=1
+#SBATCH --gpus=1
+#SBATCH --time=01:00:00 # one hour to fit
 
 # load necessary modules
 module load Miniconda3/4.9.2
 source activate hddmnn_env  # for all installed packages
 export PYTHONUNBUFFERED=TRUE # use -u to continually show output in logfile (unbuffered)
 
-# do something
-date; hostname
+# are we using the GPU?
+echo "[$SHELL] Using GPU: "$CUDA_VISIBLE_DEVICES
+echo "[$SHELL]: "$DATE
+
+# get the current working directory
+export CWD=$(pwd)
+echo "[$SHELL] CWD: "$CWD
+
+# Set the path to the python file
+export PATH_TO_PYFILE=$CWD
+echo "[$SHELL] Path of python file: "$PATH_TO_PYFILE
+
+# Set name of the python file
+export PYFILE=$CWD/fit_simdata.py
+
+# Create a directory of local scratch on the node
+echo "[$SHELL] Node scratch: "$SCRATCH
+export RUNDIR=$SCRATCH/test_tf
+mkdir $RUNDIR
+echo "[$SHELL] Run directory"$RUNDIR
+
+# Create directory for plots
+export DATADIR=$RUNDIR/data
+
+# copy script to local scratch directory and change into it
+cp $PYFILE $RUNDIR/
+cd $RUNDIR
+
+# Actuall run the file
 python fit_simdata.py -m $SLURM_ARRAY_TASK_ID # run the number of arrays thats specified
-date
+
+# Move stat directory back to CWD
+echo "[$SHELL] Copy files back to CWD"
+cp -r $DATADIR $CWD/
+
+# Wrap up
+echo "[$SHELL]: Finished at " $DATE
--- fit_simdata.py
+++ fit_simdata.py
@@ -14,7 +14,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 # Data management
 import pandas as pd
 import numpy as np
-import pickle, os, time
+import pickle, os, time, sys
 
 # Plotting
 import matplotlib.pyplot as plt
@@ -38,39 +38,50 @@ print(mypath)
 ## generate some artificial choice data with mediation variable
 ##  ============================================================= ##
 
+# parse input args
+d_num = sys.argv[1] # not used atm
+
+input_filenames =[]
+output_filenames = []
 for eff_x in ['no', 'v']: #, 'z', 'vz', 'no']:
   for eff_m in ['no', 'v']: #, 'z', 'vz', 'no']:
+      input_filenames.append('%s/data/data_df_X%s_M%s.csv'%(mypath, eff_x, eff_m))
+      output_filenames.append('%s/fit_hddmnn_X%s_M%s.csv'%(mypath, eff_x, eff_m))
 
-        t_start = time.time()
-        
-        # grab datafile from home directory (can also read from GitHub)
-        data = pd.read_csv('%s/data/data_df_X%s_M%s.csv'%(mypath, eff_x, eff_m))
-        print(data.describe())
+#%% ============================================================= ##
+## fit this dataset
+##  ============================================================= ##
 
-        # Set up the regressors
-        reg_model_v = {'model': 'v ~ 1 + S + M + X', 'link_func': lambda x: x}
-        # reg_model_z = {'model': 'z ~ 1 + prevresp', 'link_func': lambda x: x}
-        reg_descr = [reg_model_v]
-        
-        model = 'ddm' # start simple
-        n_samples = 1000
-        hddmnn_model = hddm.HDDMnnRegressor(data,
-                                   reg_descr,
-                                   model = model,
-                                   include = hddm.simulators.model_config[model]['hddm_include'],
-                                   p_outlier = 0.00,
-                                   is_group_model = True,
-                                   group_only_regressors=False,
-                                   informative = False)
-        hddmnn_model.sample(n_samples, burn = 100)
-        # 
-        md_summ = hddmnn_model.gen_stats().reset_index()
-        # df_summ = hddm.utils.results_long2wide(md_summ, name_col='index', val_col='mean')
-        
-        # see https://colab.research.google.com/notebooks/io.ipynb#scrollTo=p2E4EKhCWEC5
-        md_summ.to_csv('%s/fit_hddmnn_X%s_M%s.csv'%(mypath, eff_x, eff_m))
-        # files.download('fit_hddmnn_X%s_M%s.csv'%(eff_x, eff_m))
-        
-        elapsed = time.time() - t_start
-        print('Elapsed time: %.2f seconds' % (elapsed))
-         
+t_start = time.time()
+
+# grab datafile from home directory (can also read from GitHub)
+data = pd.read_csv(input_filenames[d_num])
+print(data.describe())
+
+# Set up the regressors
+reg_model_v = {'model': 'v ~ 1 + S + M + X', 'link_func': lambda x: x}
+# reg_model_z = {'model': 'z ~ 1 + prevresp', 'link_func': lambda x: x}
+reg_descr = [reg_model_v]
+
+model = 'ddm' # start simple
+n_samples = 1000
+hddmnn_model = hddm.HDDMnnRegressor(data,
+                           reg_descr,
+                           model = model,
+                           include = hddm.simulators.model_config[model]['hddm_include'],
+                           p_outlier = 0.00,
+                           is_group_model = True,
+                           group_only_regressors=False,
+                           informative = False)
+hddmnn_model.sample(n_samples, burn = 100)
+# 
+md_summ = hddmnn_model.gen_stats().reset_index()
+# df_summ = hddm.utils.results_long2wide(md_summ, name_col='index', val_col='mean')
+
+# see https://colab.research.google.com/notebooks/io.ipynb#scrollTo=p2E4EKhCWEC5
+md_summ.to_csv(output_filenames[d_num])
+# files.download('fit_hddmnn_X%s_M%s.csv'%(eff_x, eff_m))
+
+elapsed = time.time() - t_start
+print('Elapsed time: %.2f seconds' % (elapsed))
+ 
